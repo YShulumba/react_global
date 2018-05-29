8e7cfaedaee48e6bc438cb87d8cef8b1fc6da940
Yuliia Shulumba <Yuliia_Shulumba@epam.com>
2018-05-30 00:22:09 +0300

    Create new project with create-react-app

--- react-app/client/src/CreateElement.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-var createElement = React.createElement(
-	'h1', 
-	{className:'create-element'},
-	'This is the main title created with React.createElement'
-);
-
-export default createElement;
